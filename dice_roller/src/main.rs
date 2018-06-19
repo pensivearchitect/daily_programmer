d3b75b25cc71639630e52a81ddc051338d357c3e
Joshua Bell <josh@joshuabell.io>
2018-06-19 01:58:39 -0700

    parse inputs from stdin and finish implementation

--- dice_roller/src/main.rs
+++ dice_roller/src/main.rs
@@ -2,12 +2,36 @@
 extern crate lazy_static;
 extern crate dice_roller;
 
-use dice_roller::rng::Rng;
+use std::io;
 
-fn main() {
+use dice_roller::rng::Rng;
+use dice_roller::dice::Dice;
+
+fn main() -> io::Result<()> {
     unsafe {
         Rng::feature_check();
     }
-    println!("{}", Rng.next());
-    println!("{}", Rng.next());
+    loop {
+        let mut buffer = String::new();
+        match io::stdin().read_line(&mut buffer) {
+            Ok(_) => {
+                run(buffer.to_string());
+            },
+            Err(error) => println!("{}", error),
+        }
+    }
+    Ok(())
+}
+
+fn run(roll: String) {
+    let num: Vec<&str> = roll.trim().split(char::is_alphabetic).collect();
+    let (number, sides) = (num[0].parse::<u8>().unwrap(), num[1].parse::<u8>().unwrap());
+    let mut count = number;
+    let mut result = 0;
+    while count != 0 {
+        let die = Dice::new().sides(sides);
+        result = result + die.roll();
+        count -= 1;
+    }
+    println!("{}", result);
 }
